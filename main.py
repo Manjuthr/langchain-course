1e3ca33099f001dc5bc1bdde6a1c75cc404406fa
Manjuthr <manju.thr@gmail.com>
2026-03-21 23:02:17 +0530

    Updated search options using tavily and Tavily Search from Langchain

--- main.py
+++ main.py
@@ -10,15 +10,24 @@ from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 from langchain_community.tools import DuckDuckGoSearchRun
-from tavily import TavilyClient
+
 from langchain_ollama import ChatOllama
+
+
 
 
 ollamamodel = "qwen2.5-coder:7b"
 model = "gemini-2.5-flash"
-model = "gemini-2.5-flash-lite"
-tavily = TavilyClient()
+# model = "gemini-2.5-flash-lite"
+# tavily = TavilyClient()
 
+
+# using langchain TavilySearch -------------------------
+from langchain_tavily import TavilySearch
+tools = [TavilySearch()]
+
+# using custom developed tools using Tavily without using langchain TavilySearch -------------------------
+from tavily import TavilyClient
 @tool
 def search(query:str) -> str:
     """Tool that search over internet
@@ -28,18 +37,20 @@ def search(query:str) -> str:
     """
     print(f"Searching for {query}")
     # return "Stuttgart Weather is sunny"
-    return tavily.search(query=query)
+    return TavilyClient.search(query=query)
+tools = [search]
 
 # llm = ChatGoogleGenerativeAI()
 llm = ChatGoogleGenerativeAI(model=model,temperature=0)
 # llm = ChatOllama(model=ollamamodel,temperature=0)
-tools = [search]
 agent = create_agent(model = llm, tools = tools)
 
 def main():
     print("Hello from Long-chain")
     question = "What is the weather now in Stuttgart Germany"
-    question = "Search for 3 jobs on Teamcenter implementation from likedin posted recently"
+    question = "Search for 3 jobs on Teamcenter implementation from linkedin posted recently"
+    question = "Search for 3 jobs on Teamcenter implementation from linkedin posted"
+    question = "Search for Latest updates from news articles about current US-Iran war?"
     result = agent.invoke({"messages": HumanMessage(content=question)})
     print(result)
 
